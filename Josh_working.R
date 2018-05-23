037aa0d773b5ddaf180c5be223609ed398f09ecc
jvichare <joshvichare@gmail.com>
2018-05-23 16:04:14 -0400

    end of day EDA code

--- Josh_working.R
+++ Josh_working.R
@@ -15,21 +15,38 @@ orders$Ship.Date = as.Date(orders$Ship.Date, format = "%m/%d/%y")
 orders$Order.Date = as.Date(orders$Order.Date, format = "%m/%d/%y")
 
 qty_group = orders %>%
-  group_by(Ship.Date) %>%
+  group_by(Order.Date) %>%
   summarise(count = sum(Quantity)) %>%
-  arrange(Ship.Date)
+  arrange(Order.Date)
 
 group_cat = orders %>% 
-  group_by(Ship.Date, Category) %>% 
+  group_by(Order.Date, Category) %>% 
   summarise(count = sum(Quantity))
 
 # Part 2.1
-ggplot(data = qty_group, aes(x = Ship.Date, y = count)) + geom_line()
+ggplot(data = qty_group, aes(x = Order.Date, y = count)) + geom_smooth()
+
 # Part 2.2
-ggplot(data = group_cat, aes(x = Ship.Date, y = count)) + geom_line(aes(color = Category))
+ggplot(data = group_cat, aes(x = Order.Date, y = count)) + geom_smooth(aes(color = Category))
 
-# Alternatively
-density_plot = orders %>% 
-  group_by(Order.Date, Category)
+# Part 3.1
+returns$Order.ID = returns$`Order ID`
+returns = returns[,-2]
+returned_orders = inner_join(orders, returns, by = 'Order.ID')
 
-ggplot(density_plot, aes(Order.Date)) + geom_density(aes(color = Category))
+yearly_loss = group_by(returned_orders, year = year(Order.Date)) %>%
+  summarise(profit_loss = sum(Profit))
+
+ggplot(data = yearly_loss, aes(x = year, y = profit_loss)) + geom_bar(stat = 'identity')
+
+# Part 3.2
+num_returns = returned_orders %>%
+  group_by(Order.ID, Customer.ID) %>%
+  group_by(Customer.ID) %>% 
+  summarise(count = n()) %>% 
+  arrange(desc(count)) %>% 
+  filter(count > 1)
+
+nrow(num_returns)
+
+nrow(filter(num_returns, count > 5)) 
